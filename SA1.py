0e4a1537548289f991ea297ba842a87b770a040d
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-07-29 15:01:03 +0530

    Add files via upload

---
+++ SA1.py
@@ -0,0 +1,30 @@
+import pygame, pymunk
+import pymunk.pygame_util
+
+pygame.init()
+
+height = 600
+width = 690
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+#pymunk space
+gravity = 1000
+wind = 200
+space = pymunk.Space()
+space.gravity = wind, gravity
+draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+
+while True:
+    screen.fill((255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    
+    #space reload
+    space.step(1/60)
+    clock.tick(60)
